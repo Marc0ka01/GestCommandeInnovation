20977476cdab2b993e6942dcda35cd61720f3023
Marc0ka <okaariel01@gmail.com>
2022-12-05 13:45:34 +0000

    mise à jour

--- script.py
+++ script.py
@@ -1,3 +1,48 @@
-from tkinter import *
+import tkinter as tk
+from PIL import ImageTk
 
-t = Tk
+# initialisation de l'application
+t = tk.Tk()
+
+# titre de la fenetre
+t.title("Store Gest Ino")
+
+# positionnemt de la fenetre au centre
+t.eval("tk::PlaceWindow . center")
+x = t.winfo_screenwidth() // 4
+y = int(t.winfo_screenheight() * 0.1)
+t.geometry("1000x600+" + str(x) + '+' + str(y))
+
+# Frame 1
+frame1 = tk.Frame(t, width=1000, height=600)
+frame1.grid(row=0, column=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# execution de l'application
+t.mainloop()
